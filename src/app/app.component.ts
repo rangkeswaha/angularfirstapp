987cbae376ed39627c7fe7f95956bdc0bf49a6bf
rangkeswaha <c14180243@john.petra.ac.id>
2021-02-23 18:48:09 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  name = "Angular " + VERSION.major;
+  hasil = 0;
+  a = 0;
+  b = 0;
+
+  item = [
+    { nama: "Jeruk", harga: 15000, satuan: "kg" },
+    { nama: "Apel", harga: 5000, satuan: "kg" }
+  ];
+
+  tampil : Boolean = true;
+  tulisanopen : String;
+
+  open(){
+    this.tulisanopen = "TOKO DIBUKA"
+    alert("DIBUKA")
+  }
+
+  values = "";
+  onKey(event: any){
+    this.values += event.target.value + '|';
+  }
 }
